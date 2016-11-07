8ad1bf9de526e3b3df3355391eef0259d68384a5
Maciej Zakrzewski <Maciej Zakrzewski>
2016-11-08 00:25:31 +0100

    Dodano mozliwosc tworzenia duzych map.

--- Assets/Figure.cs
+++ Assets/Figure.cs
@@ -6,21 +6,28 @@ using UnityEngine;
 
 namespace Assets
 {
-    class Figure : MonoBehaviour
+    public class Figure : MonoBehaviour
     {
-        public Vector2 FigurePosition;
-        public FigureModel FigureModel { get; set; }
-        public Armie armie;
+        //public Vector2 FigurePosition;
+        public int CurrentX { set; get;}
+        public int CurrentY { set; get; }
+        //   public FigureModel FigureModel { get; set; }
+        //   public Armie armie;
+        public void SetPosition(int x, int y)
+        {
+            CurrentX = x;
+            CurrentY = y;
+        }
 
         public void InitializeModel()
         {
-            var figure = new GameObject();
-            figure.AddComponent<FigureModel>();
-            FigureModel = figure.GetComponent<FigureModel>();
-            figure.transform.parent = transform;
-            figure.transform.localPosition = new Vector3(0, 0, 0);
-            figure.GetComponent<Renderer>().material.shader = Shader.Find("Diffuse");
-            figure.GetComponent<Renderer>().material.mainTexture = Resources.Load("textures/hex") as Texture2D;
+    //        var figure = new GameObject();
+    //        figure.AddComponent<FigureModel>();
+    //        FigureModel = figure.GetComponent<FigureModel>();
+    //        figure.transform.parent = transform;
+    //        figure.transform.localPosition = new Vector3(0, 0, 0);
+    //        figure.GetComponent<Renderer>().material.shader = Shader.Find("Diffuse");
+    //        figure.GetComponent<Renderer>().material.mainTexture = Resources.Load("textures/hex") as Texture2D;
         }
     }
 }
