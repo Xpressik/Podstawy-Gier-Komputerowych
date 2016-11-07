eafca359c1836ec44a5f943dd41b4893b56f2581
Maciej Zakrzewski <Maciej Zakrzewski>
2016-11-07 18:25:47 +0100

    Dodano nieregularnosci przy tworzeniu hexow.

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -187,6 +187,8 @@ public class HexGrid : MonoBehaviour {
 			new Vector2(position.x, position.z);
 		label.text = cell.coordinates.ToStringOnSeparateLines();
 		cell.uiRect = label.rectTransform;
+
+        cell.Elevation = 0;
 	}
 }
 [Serializable]
