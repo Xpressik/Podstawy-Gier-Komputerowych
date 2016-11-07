b674ff487f2b9e316c824245a25d0694e573a1d7
Maciej Zakrzewski <Maciej Zakrzewski>
2016-11-08 00:13:25 +0100

    Dodano mozliwosc tworzenia duzych map przez chunki.

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -20,11 +20,13 @@ public static class HexMetrics {
 
 	public const float verticalTerraceStepSize = 1f / (terracesPerSlope + 1);
 
-    public const float cellPerturbStrength = 4f;
+	public const float cellPerturbStrength = 4f;
 
-    public const float noiseScale = 0.003f;
+	public const float elevationPerturbStrength = 1.5f;
 
-    public const float elevationPerturbStrength = 1.5f;
+	public const float noiseScale = 0.003f;
+
+	public const int chunkSizeX = 5, chunkSizeZ = 5;
 
 	static Vector3[] corners = {
 		new Vector3(0f, 0f, outerRadius),
@@ -39,7 +41,10 @@ public static class HexMetrics {
 	public static Texture2D noiseSource;
 
 	public static Vector4 SampleNoise (Vector3 position) {
-		return noiseSource.GetPixelBilinear(position.x * noiseScale, position.z * noiseScale);
+		return noiseSource.GetPixelBilinear(
+			position.x * noiseScale,
+			position.z * noiseScale
+		);
 	}
 
 	public static Vector3 GetFirstCorner (HexDirection direction) {
