6f19543126b788bd0e6eb1e67f141e813a38fb43
michal9521 <mackiewicz.m95@gmail.com>
2016-11-21 23:19:42 +0100

    test

---
+++ Assets/Mechanics/Field.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Field : MonoBehaviour {
+
+    //public Castle camp;
+    public int supply;
+    public int contraband; // waluta
+    public int garrison;
+    //public Castle castle;
+    //public Army army;
+    public Player owner;
+    public HexCell hex;
+
+    public Field()
+    {
+
+    }
+}
---
+++ Assets/Mechanics/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    private int turn;
+    private string name;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+
+}
